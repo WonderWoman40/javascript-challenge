a3e27fe9aa7b94d5a7f87f2a57d0cbf938bd294c
WonderWoman40 <67789938+WonderWoman40@users.noreply.github.com>
2021-01-11 17:53:21 -0500

    update file

--- UFO-level-1 /static/js/app.js
+++ UFO-level-1 /static/js/app.js
@@ -1,32 +1,31 @@
 // from data.js
 var tableData = data;
-var tbody = d3.select("tbody");
-function buildTable(data) {
-    tbody.html("");
-    data.forEach((dataRow) => {
-        var row = tbody.append("tr");
-
-        Object.values(dataRow).forEach((val) => {
-            let cell = row.append("td");
-            cell.text(val);
-        }
-    );
-    
-});
+// select tbody
+tbody = d3.select("tbody")
+// using Object.entries to get key, value data inside of the table
+// and loop through them to add to the table in html
+function displayData(data){
+    tbody.text("")
+    data.forEach(function(sighting){
+    new_tr = tbody.append("tr")
+    Object.entries(sighting).forEach(function([key, value]){
+        new_td = new_tr.append("td").text(value)	
+    })
+})}
+displayData(tableData)
+//select the web user's input and the filter button
+var dateInputText = d3.select("#datetime")
+var button = d3.select("filter-btn")
+// filter data with date that the user inputs
+function clickSelect(){
+    //don't refresh the page!
+    d3.event.preventDefault();
+    //print the value that was input
+    console.log(dateInputText.property("value"));
+    //create a new table showing only the filterd data
+    var new_table = tableData.filter(sighting => sighting.datetime===dateInputText.property("value"))
+    //display the new table
+    displayData(new_table);
 }
-    
-function handleClick() {
-    var date = d3.select("#datetime").property("value");
-    let filterData = tableData;
-
-    if(date) {
-
-        filterData = filterData.filter(row => row.datetime === date);
-
-    }
-    buildTable(filterData);
-}
-
-d3.selectAll("#filter-btn").on("click",handleClick);
-
-buildTable(tableData);
+// event listener to handle change and click
+dateInputText.on("change", clickSelect)
